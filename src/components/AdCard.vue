0b610baf25af2dfe67638cbdbd1f9f98311c4caa
slavcoc <viktor.bozhinoski@bestisoft.com>
2018-12-26 02:11:40 +0100

    Adcard(listing jobs ads for students)added

---
+++ src/components/AdCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card class="v-card-jobs border-round">
+    <div style="display:flex;padding:10px;">
+        <v-avatar
+          color="grey lighten-4"
+        >
+          <img :src="image" alt="avatar">
+        </v-avatar>
+      <div style="padding-left:10px;">
+        <h3>{{cardInfo.title}}</h3>
+        <p>{{cardInfo.location}}</p>
+      </div>
+    </div>
+    <div style="padding:10px;">
+      <p>{{cardInfo.shortDescription}}</p>
+    </div>
+    <div style="text-align:center;padding-bottom:10px;">
+      <v-btn dark round color="#448aff">Save</v-btn>
+      <v-btn dark round color="#448aff">Apply</v-btn>
+    </div>
+
+  </v-card>
+</template>
+
+<script>
+import image from '@/assets/logo.png'
+
+export default {
+  props: {
+    cardInfo: Object
+  },
+  data() {
+    return {
+      image
+    }
+  }
+}
+</script>
+
