c861b1144eb6b1610467826452417b3a7e5a18d3
slavcoc <viktor.bozhinoski@bestisoft.com>
2018-12-28 22:57:34 +0100

    Employe profile ready

---
+++ src/components/profile/AccountInfo.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+    v-card.border-round.content-inherit.flex-basis-20
+      v-toolbar
+        v-toolbar-title.dark-text-blue.f1-8m Account Info
+        v-spacer
+        v-dialog(v-model="dialog", width="600", persistent=true)
+          v-btn.noShadow(slot="activator", fab=true small=true)
+            v-icon create
+          v-card.border-round
+            v-container(grid-list-xl='')
+              v-layout(wrap='', align-center='')
+                v-flex(xs12='', sm12='', d-flex='')
+                  h1.label Edit Account info
+                v-flex(xs12='', sm6='', d-block='')
+                  .field-label Email
+                  v-text-field.br5(:label="fields.email" v-model="fields.email" solo="")
+                v-flex(xs12='', sm6='', d-block='')
+                  .field-label Phone Number
+                  v-text-field.br5(:label="fields.phoneNumber" v-model="fields.phoneNumber" solo="")
+                div.flex.space-around
+                  v-btn.flat.border-round(@click="cancel") Cancel
+                  v-btn.flat.border-round(@click="save") Save
+      div.p10
+        div.flex.align-center
+          p.dark-text-blue Email:
+          p {{fields.email}}
+        div.flex
+          p.dark-text-blue Phone Number:
+          p {{fields.phoneNumber}}
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      dialog: false,
+      fields: {
+        email: 'test@email.com',
+        phoneNumber: '020000',
+      }
+    }
+  },
+  mounted() {
+    this.cachedFields = Object.assign({}, this.fields);
+  },
+  methods: {
+    save() {
+      this.dialog = false;
+      this.cachedFields = Object.assign({}, this.fields);
+    },
+    cancel() {
+      this.dialog = false;
+      this.fields = Object.assign({}, this.cachedFields);
+    }
+  }
+}
+</script>
+
---
+++ src/components/profile/CompanyInfo.vue
@@ -0,0 +1,74 @@
+<template lang="pug">
+    v-card.border-round.content-inherit.flex-basis-20
+      v-toolbar
+        v-toolbar-title.dark-text-blue.f1-8m Company Information
+        v-spacer
+        v-dialog(v-model="dialog", width="600", persistent=true)
+          v-btn.noShadow(slot="activator", fab=true small=true)
+            v-icon create
+          v-card.border-round
+            v-container(grid-list-xl='')
+              v-layout(wrap='', align-center='')
+                v-flex(xs12='', sm12='', d-flex='')
+                  h1.label Edit Company info
+                v-flex(xs12='', sm6='', d-block='')
+                  .field-label Company name
+                  v-text-field.br5(:label="fields.name" v-model="fields.name" solo="")
+                v-flex(xs12='', sm6='', d-block='')
+                  .field-label Company size
+                  v-select(:items='companySize', background-color='#f5f5f5', v-model='fields.size', :label='fields.size',item-text='title', solo='')
+                v-flex(xs12='', sm6='', d-block='')
+                  .field-label Office Location
+                  v-text-field.br5(:label="fields.location" v-model="fields.location" solo="")
+                div.flex.space-around
+                  v-btn.flat.border-round(@click="cancel") Cancel
+                  v-btn.flat.border-round(@click="save") Save
+      div.p10
+        div.flex.align-center
+          p.dark-text-blue Company Name:
+          p {{fields.name}}
+        div.flex
+          p.dark-text-blue Company Size:
+          p {{fields.size}}
+        div.flex
+          p.dark-text-blue Office Location:
+          p {{fields.location}}
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      dialog: false,
+      fields: {
+        name: 'University of Toronto',
+        size: '11-50',
+        location: 'Toronto, ON'
+      },
+      companySize: [{
+        id: 0,
+        title: '11-50'
+      }, {
+        id: 1,
+        title: '50-100'
+      }]
+    }
+  },
+  mounted() {
+    this.cachedFields = Object.assign({}, this.fields);
+  },
+  methods: {
+    save() {
+      this.dialog = false;
+      this.cachedFields = Object.assign({}, this.fields);
+    },
+    cancel() {
+      this.dialog = false;
+      this.fields = Object.assign({}, this.cachedFields);
+    }
+  }
+}
+</script>
+
