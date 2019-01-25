87bad96e17da4ba6f0df97315baffbce9cde52f0
Darko <darko.simonovski@bestisoft.com>
2019-01-25 21:11:58 +0100

    Created sign up popup

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,17 +3,27 @@
       <v-toolbar-side-icon class="h-img"><v-img :src="image"></v-img></v-toolbar-side-icon>
       <v-toolbar-title class="dark-text-blue f3em bold-weight">employo</v-toolbar-title>
       <v-spacer></v-spacer>
-       <v-dialog content-class="dialog-round" v-model="dialog" width="50%" >
-      <v-btn flat slot="activator" class="dark-text-blue bold-weight f1-8m">
-        Contact us
-      </v-btn>
-      <v-card>
-       <contact-pop-up @success='allowContinue($event)'></contact-pop-up>
-      </v-card>
-    </v-dialog>
+      <v-dialog content-class="dialog-round" v-model="dialogContact" width="50%" >
+        <v-btn flat slot="activator" class="dark-text-blue bold-weight f1-8m">
+          Contact us
+        </v-btn>
+        <v-card>
+          <contact-pop-up @success="dialogContact=false"></contact-pop-up>
+        </v-card>
+      </v-dialog>
+      <v-dialog content-class="dialog-round ovf-visible" v-model="dialogSignup" width="40%" >
+        <v-btn flat slot="activator" class="dark-text-blue bold-weight f1-8m">
+          Sign up
+        </v-btn>
+        <v-card class="dialog-round">
+          <v-card-text>
+            <SignUp @success="dialogSignup=false"></SignUp>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
       <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn class="dark-text-blue bold-weight f1-8m" v-for="(btn, index) in navigationBtns" @click="getRoute(btn)" :key="index" flat>{{btn}}</v-btn>
-      <v-btn class="dark-text-blue bold-weight f1-8m" flat>{{user}}</v-btn>
+        <v-btn class="dark-text-blue bold-weight f1-8m" v-for="(btn, index) in navigationBtns" @click="getRoute(btn)" :key="index" flat>{{btn}}</v-btn>
+        <v-btn v-if="user" class="dark-text-blue bold-weight f1-8m" flat>{{user}}</v-btn>
       </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -21,6 +31,7 @@
 <script>
 import image from '@/assets/EmployoLogo.svg'
 import ContactPopUp from '@/components/ContactPopUp'
+import SignUp from '@/components/SignUp'
 import router from '@/router'
 
 export default {
@@ -35,20 +46,19 @@ export default {
     return {
       image,
       user: null,
-      dialog: false
+      dialogContact: false,
+      dialogSignup: false
     }
   },
   methods: {
-    allowContinue(e) {
-      this.dialog = !e
-    },
     getRoute(btn) {
       const route = btn.replace(/\s/g, '').toLowerCase();
       router.push({ path: route })
     }
   },
   components: {
-    ContactPopUp
+    ContactPopUp,
+    SignUp
   }
 }
 </script>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .flex-col
+    .flex-row.justify-center
+      v-img.overlay.br50(:src="logo" width="150px")
+    .flex-row.justify-center.align-center
+      .w90.p50-top
+        v-text-field(placeholder="Email")
+        v-text-field(placeholder="Password")
+        v-text-field(placeholder="Confirm Password")
+        br
+        v-btn(round color="primary" block) Sign up
+</template>
+
+<script>
+import logo from '@/assets/EmployoLogo.svg'
+
+export default {
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+  computed: {
+    logo() {
+      return logo
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .overlay {
+    position: absolute;
+    top: -60px;
+    background-color: white;
+    box-shadow: 0 0 12px rgba(0, 0, 0, 0.6);
+    z-index: 1;
+  }
+</style>
